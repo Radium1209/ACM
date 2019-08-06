d401efc35453b61fd4959bd8bf2c119a707edcbe
Radium1209 <Radium_1209@outlook.com>
2019-08-06 12:21:28 +0800

    Add new code.

---
+++ HDU/1689.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <iostream>
+#define lson l,m,rt<<1
+#define rson m+1,r,rt<<1|1
+#define MAXN 100005
+using namespace std;
+
+int T,N,Q,x,y,z;
+int sum[MAXN<<2],lazy[MAXN<<2];
+
+void pushup(int rt) {
+    sum[rt]=sum[rt<<1]+sum[rt<<1|1];
+}
+
+void pushdown(int rt,int m) {
+    if (lazy[rt]) {
+        lazy[rt<<1]=lazy[rt<<1|1]=lazy[rt];
+        sum[rt<<1]=lazy[rt]*(m-(m>>1));
+        sum[rt<<1|1]=lazy[rt]*(m>>1);
+        lazy[rt]=0;
+    }
+}
+
+void build(int l,int r,int rt) {
+    lazy[rt]=0;
+    if (l==r) {
+        sum[rt]=1;
+        return;
+    }
+    int m=(l+r)>>1;
+    build(lson);
+    build(rson);
+    pushup(rt);
+}
+
+void update(int L,int R,int z,int l,int r,int rt) {
+    if (L<=l && r<=R) {
+        lazy[rt]=z;
+        sum[rt]=z*(r-l+1);
+        return;
+    }
+    pushdown(rt,r-l+1);
+    int m=(l+r)>>1;
+    if (L<=m) update(L,R,z,lson);
+    if (m<R) update(L,R,z,rson);
+    pushup(rt);
+}
+
+int query(int L,int R,int l,int r,int rt) {
+    if (L<=l && r<=R) {
+        return sum[rt];
+    }
+    pushdown(rt,r-l+1);
+    int m=(l+r)>>1;
+    int ret=0;
+    if (L<=m) ret+=query(L,R,lson);
+    if (m<R) ret+=query(L,R,rson);
+    return ret;
+
+}
+
+int main() {
+    cin >> T;
+    for (int cnt=1;cnt<=T;cnt++) {
+        scanf("%d%d",&N,&Q);
+        build(1,N,1);
+        while(Q--) {
+            scanf("%d%d%d",&x,&y,&z);
+            update(x,y,z,1,N,1);
+        }
+        printf("Case %d: The total value of the hook is %d.\n",cnt,query(1,N,1,N,1));
+    }
+    return 0;
+}

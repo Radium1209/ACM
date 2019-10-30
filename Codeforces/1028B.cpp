505c9f3aae9fe53e70f7b554116f2d85ed2c8784
Radium1209 <Radium_1209@outlook.com>
2019-10-30 17:06:36 +0800

    Add new code.

---
+++ Codeforces/1028B.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int n,m;
+
+int main() {
+    cin >> n >> m;
+    for (int i=1;i<=2230;i++)
+        printf("5");
+    printf("\n");
+    for (int i=1;i<=2229;i++)
+        printf("4");
+    printf("5\n");
+    return 0;
+}
